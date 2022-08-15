6631f444e0e17bedc9e5ff6cb1c6e070a4c37263
Ruslan Kostoev <ruslancmc@gmail.com>
2022-08-15 16:00:39 +0300

    fix bug in annual and monthly constraints

--- libecole/src/instance/ship-allocation.cpp
+++ libecole/src/instance/ship-allocation.cpp
@@ -13,8 +13,8 @@
 #include <xtensor/xtensor.hpp>
 #include <xtensor/xview.hpp>
 
-#include <xtensor/xcsv.hpp>
 #include <xtensor/xnpy.hpp>
+#include <xtensor/xio.hpp>
 
 #include "ecole/instance/ship-allocation.hpp"
 #include "ecole/scip/cons.hpp"
@@ -46,8 +46,8 @@ namespace {
 
 using std::size_t;
 
-SCIP_VAR* add_var(SCIP * scip, std::string name, SCIP_Real cost, SCIP_Vartype vartype) {
-	auto unique_var = scip::create_var_basic(scip, name.c_str(), 0, 1, cost, vartype);
+SCIP_VAR* add_var(SCIP * scip, std::string name, SCIP_Real cost, SCIP_Real lb, SCIP_Real ub, SCIP_Vartype vartype) {
+	auto unique_var = scip::create_var_basic(scip, name.c_str(), lb, ub, cost, vartype);
 	SCIP_VAR* var_ptr = unique_var.get();
 	scip::call(SCIPaddVar, scip, var_ptr);
 	return var_ptr;
@@ -63,9 +63,9 @@ xt::xtensor<SCIP_VAR*, 4> add_vars(SCIP* scip, xt::xtensor<SCIP_Real, 3> c) {
 	for (size_t i = 0; i < shape[1]; ++i) {
 		for (size_t j = 0; j < shape[2]; ++j) {
 			for (size_t k = 0; k < shape[3]; ++k) {
-				vars(0, i, j, k) = add_var(scip, fmt::format("x_{}_{}_{}", i, j, k), 0.0, 		 SCIP_VARTYPE_BINARY);
-				vars(1, i, j, k) = add_var(scip, fmt::format("q_{}_{}_{}", i, j, k), 0.0, 		 SCIP_VARTYPE_CONTINUOUS);
-				vars(2, i, j, k) = add_var(scip, fmt::format("y_{}_{}_{}", i, j, k), c(i, j, k), SCIP_VARTYPE_CONTINUOUS);
+				vars(0, i, j, k) = add_var(scip, fmt::format("x_{}_{}_{}", i, j, k), 0.0, 		 0, 1, SCIP_VARTYPE_BINARY);
+				vars(1, i, j, k) = add_var(scip, fmt::format("q_{}_{}_{}", i, j, k), 0.0, 		 0, 1, SCIP_VARTYPE_CONTINUOUS);
+				vars(2, i, j, k) = add_var(scip, fmt::format("y_{}_{}_{}", i, j, k), c(i, j, k), 0, 1, SCIP_VARTYPE_CONTINUOUS);
 			}
 		}
 	}
@@ -101,32 +101,32 @@ auto add_constaints(
 				name = fmt::format("obj_to_cons_pos_{}_{}_{}_term", i, j, k);
 				auto cons = scip::create_cons_non_linear(scip, name.c_str(),
 													1, &vars(2, i, j, k), lincoefs.data(),
-													1, &vars(0, i, j, k), &vars(1, i, j, k), quadcoefs.data(), -0.00001, 0.00001);
+													1, &vars(0, i, j, k), &vars(1, i, j, k), quadcoefs.data(), 0, 0);
 				scip::call(SCIPaddCons, scip, cons.get());
 
 				// =======================================================================
 
-				xt::xtensor<SCIP_VAR*, 1> x_ik = xt::view(vars, 1, i, xt::all(), k);
+				xt::xtensor<SCIP_VAR*, 1> x_ik = xt::view(vars, 0, i, xt::all(), k);
 				name = fmt::format("S_{}_{}", i, k);
-				auto coefs = xt::xtensor<SCIP_Real, 1>({vars.shape(2)}, 1.);;
+				auto coefs = xt::xtensor<SCIP_Real, 1>({vars.shape(2)}, 1.);
 				cons = scip::create_cons_basic_linear(scip, name.c_str(),
-													  x_ik.size(), &x_ik(0), coefs.data(), neg_inf, schedule(i, k));
+													  x_ik.size(), &x_ik(0), coefs.data(), 0, schedule(i, k));
 				scip::call(SCIPaddCons, scip, cons.get());
 			}
 
-			xt::xtensor<SCIP_VAR*, 1> x_ij = xt::view(vars, 1, i, j, xt::all());
+			xt::xtensor<SCIP_VAR*, 1> x_ij = xt::view(vars, 2, i, j, xt::all());
 			name = fmt::format("M_{}_{}", i, j);
-			auto coefs = xt::view(max_prod, i, j, xt::all());
+			xt::xarray<SCIP_Real> coefs = xt::view(max_prod, i, j, xt::all());
 			auto cons = scip::create_cons_basic_linear(
-				scip, name.c_str(), x_ij.size(), &x_ij(0), coefs.data(), neg_inf, month_constr(i, j));
+				scip, name.c_str(), x_ij.size(), &x_ij(0), &coefs(0), 0, month_constr(i, j));
 			scip::call(SCIPaddCons, scip, cons.get());
 		}
 
-		xt::xtensor<SCIP_VAR*, 1> x_j = xt::flatten(xt::view(vars, 1, xt::all(), j, xt::all()));
+		xt::xtensor<SCIP_VAR*, 1> x_j = xt::flatten(xt::view(vars, 2, xt::all(), j, xt::all()));
 		name = fmt::format("A_{}", j);
-		xt::xtensor<SCIP_Real, 1> coefs = xt::flatten(xt::view(max_prod, xt::all(), j, xt::all()));
+		xt::xarray<SCIP_Real> coefs = xt::flatten(xt::view(max_prod, xt::all(), j, xt::all()));
 		auto cons = scip::create_cons_basic_linear(
-			scip, name.c_str(), x_j.size(), &x_j(0), coefs.data(), neg_inf, annual_constr(j));
+			scip, name.c_str(), x_j.size(), &x_j(0), &coefs(0), 0, annual_constr(j));
 		scip::call(SCIPaddCons, scip, cons.get());
 	}
 }
@@ -157,6 +157,13 @@ scip::Model SAPGenerator::generate_instance(Parameters parameters, RandomGenerat
 	// xt::xtensor<SCIP_Real, 1> annual_constr = xt::load_npy<SCIP_Real>(a_path);
 	// xt::xtensor<SCIP_Real, 2> schedule = xt::load_npy<SCIP_Real>(s_path);
 
+	xt::xarray<double> a = {1, 5, 9};
+	auto b = xt::diag(a, 1);
+	std::ofstream myfile;
+	myfile.open("test.txt");
+	myfile << b << std::endl;
+	myfile.close();
+
 	
 	// sample returns
 	xt::xtensor<SCIP_Real, 3> availability = xt::random::binomial<size_t>(shape, 1, 0.8, rng) + 0;
@@ -177,7 +184,7 @@ scip::Model SAPGenerator::generate_instance(Parameters parameters, RandomGenerat
 	// annual constaints
 	std::uniform_real_distribution<double> unif(0.6, 0.9);
 	std::default_random_engine re;
-	double beta = unif(re);
+	double beta = unif(rng);
 	xt::xtensor<SCIP_Real, 1> annual_constr = beta * xt::sum(month_constr, {0});
 
 	xt::xtensor<SCIP_Real, 2> schedule = xt::random::binomial<size_t>({n_months, n_ships}, 1, 0.9, rng) + 0;
